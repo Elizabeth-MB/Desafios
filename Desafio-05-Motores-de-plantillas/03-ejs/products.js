5aec33289aac35737fd78cfd4d9ff74f929cb0b2
Elizabeth Martinez <marieli.martinezb@gmail.com>
2022-09-15 10:43:53 -0500

    Motor de plantillas - EJS - Finalizado

--- Desafio-05-Motores-de-plantillas/03-ejs/products.js
+++ Desafio-05-Motores-de-plantillas/03-ejs/products.js
@@ -1,22 +1,22 @@
 let products = [
-    {
+    /* {
         id: 1,
-        title: 'Sopa Instantanea',
+        title: 'Burger',
         price: 20,
-        thumbnail: 'https://cdn2.iconfinder.com/data/icons/fruits-and-vegetables-25/85/apple_fruit_food-128.png'
+        thumbnail: 'https://cdn3.iconfinder.com/data/icons/world-cuisine-astute-vol-1/512/Burger_Cheeseburger-256.png'
     },
     {
         id: 2,
-        title: 'Leche',
+        title: 'Orange',
         price: 20,
-        thumbnail: 'https://cdn2.iconfinder.com/data/icons/fruits-and-vegetables-25/85/apple_fruit_food-128.png'
+        thumbnail: 'https://cdn4.iconfinder.com/data/icons/fruits-79/48/08-orange-256.png'
     },
     {
         id: 3,
-        title: 'Arroz',
+        title: 'Apple',
         price: 20,
-        thumbnail: 'https://cdn2.iconfinder.com/data/icons/fruits-and-vegetables-25/85/apple_fruit_food-128.png'
-    },
+        thumbnail: 'https://cdn4.iconfinder.com/data/icons/fruits-79/48/19-green_apple-256.png'
+    }, */
 ]
 
 module.exports = products
